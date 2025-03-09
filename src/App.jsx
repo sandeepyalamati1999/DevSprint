85a815a1335e97ddc260bbb902a6f1c0fec1faed
Sandeep <sandeepyalamati1999@gmail.com>
2025-03-09 21:35:30 +0530

    app chngs

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
                
                 <main>
                     <Routes>
-                        <Route path="/" element={<Login />} />
+                        <Route path="/" element={<Login />} /> 
                     </Routes>
                 </main>
             </div>
